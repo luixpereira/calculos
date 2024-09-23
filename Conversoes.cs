32fdf7bc8fd6abb03d9b5c749912d5865693a9a1
luixpereira <180971986+luixpereira@users.noreply.github.com>
2024-09-23 21:41:56 +0100

    9º Commit: Acrescentar código para testar

--- Conversoes.cs
+++ Conversoes.cs
@@ -5,3 +5,30 @@
     /// <param name="temperatura">A temperatua a converter.</param>
     /// <returns>Retorna o resultado da conversão da temperatura.</returns>
     /// 
+    internal static class Conversoes
+    {
+        /// <summary>
+        /// Tipo de conversão de temperatura a executar.
+        /// </summary>
+        public enum ConversaoTemperatura
+        {
+            Nulo = 0,
+            CelsiusFahrenheit,
+            FahrenheitCelsius
+        }
+
+
+        public double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+        {
+            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
+            {
+                return (temperatura * 1.8000 + 32);
+            }
+            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
+            {
+                return ((temperatura - 32) / 1.8000);
+            }
+
+            return -1;
+        }
+    }
