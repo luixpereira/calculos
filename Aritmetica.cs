bc1ce2ab133aeec79d211db751046442887d8f42
luixpereira <180971986+luixpereira@users.noreply.github.com>
2024-09-23 21:43:02 +0100

    Commit: Adicionar o método Multiplicar()

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -24,29 +24,15 @@
         }
 
         /// <summary>
-        /// Tipo de conversão de temperatura a executar.
+        /// Operação de multiplicação.
         /// </summary>
-        public enum ConversaoTemperatura
+        /// <returns>Retorna o resultado da multiplicação de dois números.</returns>
+        public static int Multiplicar(int x, int y) 
         {
-            Nulo = 0,
-            CelsiusFahrenheit,
-            FahrenheitCelsius
+            return x * y;
         }
 
 
-        public double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
-        {
-            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
-            {
-                return (temperatura * 1.8000 + 32);
-            }
-            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
-            {
-                return ((temperatura - 32) / 1.8000);
-            }
-
-            return -1;
-        }
     
 }
 
